3f92e3830c59539f56f2b161d9ad23067a8a6578
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2018-12-20 11:43:57 +0900

    first commit

---
+++ Object Repository/Page_RSS Feeds - FIFA.com/a_FIFA World Cup Russia 2018.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_FIFA World Cup Russia 2018</name>
+   <tag></tag>
+   <elementGuidId>7c88288f-598f-4413-b2be-f89db22375a7</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//div[id='rssTab']//a[text()='FIFA World Cup Russia 2018']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//div[@id='rssTab']//a[text()='FIFA World Cup Russia 2018']</value>
+   </webElementProperties>
+</WebElementEntity>
