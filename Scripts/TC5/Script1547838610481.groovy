b4a682c1ed60e3f831e116208ea5e2b12545d049
kazurayam <kazuaki.urayama@gmail.com>
2019-01-19 04:33:32 +0900

    add TC5

--- Scripts/TC5/Script1547838610481.groovy
+++ Scripts/TC5/Script1547838610481.groovy
@@ -13,21 +13,16 @@ WebUI.openBrowser('')
 WebUI.navigateToUrl('https://kazurayam.github.io/SavingAnXmlFileRatherThanDisplayingIt/')
 WebUI.verifyElementPresent(findTestObject('Page_Sample/div_resultDiv'), 10)
 
-// grasp <tr> elements : 1st way; using Katalon API with a TestObject constructed beforehand 
-List<WebElement> trElements1 = WebUI.findWebElements(
-	findTestObject("Page_Sample/div_resultDiv.table.tbody.tr"), 10)
-println "trElements1.size()=${trElements1.size()}"
-
-// grasp <tr> elements : 2nd way; using Katalon API with a TestObject constructed on the fly
-List<WebElement> trElements2 = WebUI.findWebElements(
-	new TestObject().addProperty("xpath", ConditionType.EQUALS,
-							"//div[@id='resultDiv']/table/tbody/tr"), 10)
-println "trElements2.size()=${trElements2.size()}"
-
 // grasp <tr> elements : 3rd way; using WebDriver's native API
 WebDriver driver = DriverFactory.getWebDriver()
-List<WebElement> trElements3 = driver.findElements(By.xpath("//div[@id='resultDiv']/table/tbody/tr"))
-println "trElements3.size()=${trElements3.size()}"
+WebElement webElement = driver.findElement(By.xpath("//div[@id='resultDiv']/table/tbody/tr[1]/td[1]"))
+String content1 = webElement.getText()
+println "content1=${content1}"
 
+// get text of row 1- column 1 : by Katalon API
+String content2 = WebUI.getText(
+	new TestObject().addProperty("xpath", ConditionType.EQUALS,
+							"//div[@id='resultDiv']/table/tbody/tr[1]/td[1]"))
+println "content2=${content2}"
 
 WebUI.closeBrowser()
